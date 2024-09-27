5379d0430a5efada5109453b02b767667f90cf24
vivek taware <vivektaware16@gmail.com>
2024-09-27 11:16:59 +0530

    album

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { AlbumComponent } from './component/album/album.component';
+import { MarathisongComponent } from './component/marathisong/marathisong.component';
+import { EnlishsongComponent } from './component/enlishsong/enlishsong.component';
+import { HindisongComponent } from './component/hindisong/hindisong.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path:"marathi",component:MarathisongComponent},
+    {path:"enlish",component:EnlishsongComponent},
+    {path:"hindi",component:HindisongComponent}
+
+
+];
