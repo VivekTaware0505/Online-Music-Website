8b949c4cea1b3e5c34443782065dfd63772ee14b
Sunny-PC <sunnysul57@gmail.com>
2024-10-03 00:45:16 +0530

    sunny edith v1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,16 @@ import { AlbumComponent } from './component/album/album.component';
 import { MarathisongComponent } from './component/marathisong/marathisong.component';
 import { EnlishsongComponent } from './component/enlishsong/enlishsong.component';
 import { HindisongComponent } from './component/hindisong/hindisong.component';
+import { MainComponent } from './component/main/main.component';
 
 export const routes: Routes = [
 
-    {path:"marathi",component:MarathisongComponent},
-    {path:"enlish",component:EnlishsongComponent},
-    {path:"hindi",component:HindisongComponent}
+
+    { path: "", component: MainComponent },
+    { path: "album", component: AlbumComponent},
+    { path: "marathi", component: MarathisongComponent },
+    { path: "english", component: EnlishsongComponent },
+    { path: "hindi", component: HindisongComponent }
 
 
 ];
